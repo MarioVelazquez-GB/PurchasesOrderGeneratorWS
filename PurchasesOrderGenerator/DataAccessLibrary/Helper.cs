79ceae31074825fe9c8e37f9a7b8571e743b5480
Mario Velazquez <mario.velazquez@glennonbrothers.ie>
2020-09-15 23:59:20 +0100

    -Added a Method public static string GetConnectionString(string databaseConnection) to read the appsetting and return the connection string value on the class Helper. -Added the Dev and Prod Databases connection string onto appsettings.json

--- PurchasesOrderGenerator/DataAccessLibrary/Helper.cs
+++ PurchasesOrderGenerator/DataAccessLibrary/Helper.cs
@@ -4,13 +4,31 @@ using System.Text;
 //Libraries to Read the Tokens from the appsettings.json
 using Microsoft.Extensions.Configuration;
 using System.IO;
-
+using System.Data;
 
 namespace DataAccessLibrary
 {
 	public static class Helper
 	{
-
-
+		/// <summary>
+		/// Read the appsettings.json and get the connection string
+		/// </summary>
+		/// <param name="databaseConnection">string</param>
+		/// <returns>string</returns>
+		public static string GetConnectionString(string databaseConnection)
+		{
+			try
+			{
+				var configurationBuilder = new ConfigurationBuilder();
+				var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+				configurationBuilder.AddJsonFile(path, false);
+				var root = configurationBuilder.Build();
+				return root.GetSection("Sharepoint").GetSection(databaseConnection).Value;
+			}
+			catch
+			{
+				return string.Empty;
+			}
+		}
 	}
 }
