59662ef3475298e61423b28c1de82db38065f3a0
Mario Velazquez <mario.velazquez@glennonbrothers.ie>
2020-09-15 23:26:19 +0100

    -Added new Project WS called PurchasesOrderGeneratorWS. -Added the Appsettings.json and the value for the Connection string. -Adde the Nugets Packages in order to read the appsetting.json

---
+++ PurchasesOrderGenerator/DataAccessLibrary/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Libraries to Read the Tokens from the appsettings.json
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+
+namespace DataAccessLibrary
+{
+	public static class Helper
+	{
+
+
+	}
+}
